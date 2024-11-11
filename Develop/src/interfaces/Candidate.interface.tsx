0295e7c81543e11e25de821ff35ccc454dd94bea
Kbasius <kbasius@gmail.com>
2024-11-10 20:18:50 -0500

    Gitignore Fix
    Figured out the gitignore issue.
    
    The ignore file was not at root level.

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,4 +1,9 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 export interface Candidate {
-    avatar_
+    avatar_url: string;
+    name: string;
+    location: string;
+    email: string;
+    company: string;
+    bio: string;
 }
